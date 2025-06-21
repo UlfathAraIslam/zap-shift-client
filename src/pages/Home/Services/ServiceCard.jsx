2f336265fb846ccc5629625170ef86354f9098bd
UlfathAraIslam <mouliajsahid@gmail.com>
2025-06-21 23:03:01 +0900

    add hover over service card

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const ServiceCard = ({ service}) => {
-    const {icon: Icon, title,description} = service;
+const ServiceCard = ({ service }) => {
+  const { icon: Icon, title, description } = service;
+
   return (
-    <div className="card bg-base-100 shadow-md hover:shadow-xl transition duration-300 border">
+    <div className="card bg-base-100 shadow-md border transition duration-300 group hover:bg-[#CAEB66] hover:border-[#CAEB66]">
       <div className="card-body items-center text-center">
-        <Icon className="text-4xl text-primary mb-4" />
-        <h3 className="card-title text-lg font-bold">{title}</h3>
+        <Icon className="text-4xl text-primary mb-4 transition-colors duration-300 group-hover:text-[#FFFF]" />
+
+        <h3 className="card-title text-lg font-bold transition-colors duration-300 group-hover:text-[#03373D]">
+          {title}
+        </h3>
+
         <p className="text-sm text-gray-600">{description}</p>
       </div>
     </div>
