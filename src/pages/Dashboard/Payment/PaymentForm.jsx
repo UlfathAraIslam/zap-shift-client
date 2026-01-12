1328422884d2a490af566139adcca0c01beb6c41
UlfathAraIslam <mouliajsahid@gmail.com>
2026-01-12 09:20:18 +0900

    Get Parcel Info Using TanStack Query

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,16 +1,29 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { useParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const {parcelId} = useParams();
-  console.log(parcelId);
+  const axiosSecure = useAxiosSecure();
 
   const [error, setError] = useState('');
 
-  
+  const {isPending,data: parcelInfo = {}} = useQuery({
+    queryKey: ['parcels', parcelId],
+    queryFn: async() => { 
+      const res = await axiosSecure.get(`/parcels/${parcelId}`);
+      return res.data;
+    }
+  })
+  if (isPending) {
+    return '...loading'
+  }
+  console.log(parcelInfo);
+  const amount = parcelInfo.cost;
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -43,7 +56,7 @@ const PaymentForm = () => {
             type="submit"
             disabled={!stripe}
           >
-            Pay For Prcel Pickup
+            Pay ${amount}
           </button>
           {
             error && <p className="text-red-500">{error}</p>
