27eb441584b61aa5d31b2db2d9868f6c7055f12a
UlfathAraIslam <mouliajsahid@gmail.com>
2026-01-13 17:41:48 +0900

    Create track parcel component

--- src/pages/Dashboard/Payment/__payment_steps__.js
+++ src/pages/Dashboard/Payment/__payment_steps__.js
@@ -1,8 +1,9 @@
 /**
  * 1. which payment gateway----stripe
- * 2. payment form
- * 
- * 
- * 
+ * 2. Format a payment form
+ *  3. Validate card information
+ * 4. create a payment intent in the server
+ * 5. Confirm payment 
+ *  6. after payment is successful: update payment status on the parcel also enter an entry in the parcel history collection.
  * 
  */
---
+++ src/pages/Dashboard/TrackParcel/TrackParcel.jsx
@@ -0,0 +1,11 @@
+const TrackParcel = () => {
+
+
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+        <h2>TO DO: Track parcel </h2>
+    </div>
+  );
+};
+
+export default TrackParcel;
